8a875a1aeafed9eb11e873deefbd861ffd4396e4
xiaoying <xiaoying05@kuaishou.com>
2021-03-10 11:55:02 +0800

    feat(init)

---
+++ README.md
@@ -0,0 +1,50 @@
+# eslint-plugin-shuai-lint
+
+测试用的eslint
+
+## Installation
+
+You'll first need to install [ESLint](http://eslint.org):
+
+```
+$ npm i eslint --save-dev
+```
+
+Next, install `eslint-plugin-shuai-lint`:
+
+```
+$ npm install eslint-plugin-shuai-lint --save-dev
+```
+
+
+## Usage
+
+Add `shuai-lint` to the plugins section of your `.eslintrc` configuration file. You can omit the `eslint-plugin-` prefix:
+
+```json
+{
+    "plugins": [
+        "shuai-lint"
+    ]
+}
+```
+
+
+Then configure the rules you want to use under the rules section.
+
+```json
+{
+    "rules": {
+        "shuai-lint/rule-name": 2
+    }
+}
+```
+
+## Supported Rules
+
+* Fill in provided rules here
+
+
+
+
+
---
+++ tests/lib/rules/settimeout-no-number.js
@@ -0,0 +1,45 @@
+/**
+ * @fileoverview 第二个参数禁止是数字
+ * @author shuaishuai
+ */
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+var rule = require("../../../lib/rules/settimeout-no-number"),
+
+    RuleTester = require("eslint").RuleTester;
+
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+var ruleTester = new RuleTester({
+    parserOptions: {
+        ecmaVersion: 7, // 默认支持语法为 es5
+    }
+});
+ruleTester.run("settimeout-no-number", rule, {
+
+    valid: [
+        {
+            code: 'let someNumber = 1000; setTimeout(() => { console.log(11); }, someNumber)'
+        }, {
+            code: 'setTimeout(() => { console.log(11) }, someNumber)'
+        }
+        // give me some code that won't trigger a warning
+    ],
+
+    invalid: [
+        {
+            code: "setTimeout(() => { console.log(22); }, 1000)",
+            errors: [{
+                message: "setTimeout 第二个参数禁止是数字",
+                type: "CallExpression" // rule 监听的对应钩子
+            }]
+        }
+    ]
+});
